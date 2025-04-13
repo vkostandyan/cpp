869528e26cb6f082a1cced1d09e28f864b33f602
vkostandyan <v.kostandyan05@gmail.com>
2025-04-13 14:34:18 +0400

    Add tests module06/ex01

--- module_06/ex01/main.cpp
+++ module_06/ex01/main.cpp
@@ -6,6 +6,8 @@ int main()
     tmp.c = 'a';
     tmp.i = 1;
 	Data *ptr = &tmp;
+    std::cout << "i = " << ptr->i << std::endl;
+    std::cout << "c = " << ptr->c << std::endl;
 	try
 	{
 		uintptr_t num = Serializer::serialize(ptr);
@@ -14,6 +16,10 @@ int main()
         std::cout << "Num == " << num << std::endl;
 		std::cout << "Ptr == " << ptr << std::endl;
 		std::cout << "New Ptr == " << new_ptr << std::endl;
+        new_ptr->c = 'k';
+        new_ptr->i = 20;
+        std::cout << "i = " << new_ptr->i << std::endl;
+        std::cout << "c = " << new_ptr->c << std::endl;
 	}
 	catch (const std::exception &e)
 	{
